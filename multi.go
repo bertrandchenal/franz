3d73ec46082c7710b137f91d6ec309d8fbea67ea
Bertrand Chenal <bertrand@adimian.com>
2017-09-08 09:44:47 +0200

    add waiter signal

--- multi.go
+++ multi.go
@@ -2,36 +2,39 @@ package main
 
 import "time"
 
-func Waiter(sub chan int, value int) {
+func Waiter(sub chan int, wait_signal chan int, sub_id, value int) {
 	// if unsuscribe was done too late
     defer func() {
         if r := recover(); r != nil {
-            println("Recovered in f", r)
+            println("Recovered in f", r, sub_id)
         }
     }()
 
 	select {
-	case sub <- value:
+	case sub <- value + sub_id:
 		return
 	case <- time.After(500 * time.Millisecond):
-		println("waiter timeout (TODO unsubscribe)!")
-		// IDEA, either publisher pass the signal chan to the waiters (and let them send an signal to unsubscribe the subsciber or use a lock that is used around the subsciber list
+		println("waiter timeout!")
+		wait_signal <- sub_id
 		close(sub)
 		return
 	}
 }
 
-func Publisher(signal chan struct{}, subscribers []chan int) {
+func Publisher(pub_signal chan struct{}, subscribers []chan int) {
+	wait_signal := make(chan int)
 	tick := time.Tick(100 * time.Millisecond)
 	step := 0
 	for {
 		select {
+		case died_sub := <- wait_signal:
+			println("TODO", died_sub)
 		case <- tick:
 			for i, s := range(subscribers) {
-				go Waiter(s, i + step)
+				go Waiter(s, wait_signal, i, step)
 			}
 			step += 1
-		case <-signal:
+		case <-pub_signal:
 			println("pub done")
 			return
 		}
@@ -49,17 +52,17 @@ func Subscriber(in chan int) {
 
 
 func main() {
-	signal := make(chan struct{})
+	pub_signal := make(chan struct{})
 	sub_a := make(chan int)
 	sub_b := make(chan int)
 	subs := []chan int{sub_a, sub_b}
-	go Publisher(signal, subs)
+	go Publisher(pub_signal, subs)
 	go Subscriber(sub_a)
 	go Subscriber(sub_b)
 
 
 	time.Sleep(3e9)
-	close(signal)
+	close(pub_signal)
 	time.Sleep(1e9)
 	println("main done")
 }
