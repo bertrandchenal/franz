0a36c5d5dd5cfdf57e126c710f412cc1967e72b8
Bertrand Chenal <bertrandchenal@gmail.com>
2017-09-13 16:53:49 +0200

    Implement Tube.Read

--- tube.go
+++ tube.go
@@ -1,7 +1,7 @@
 package franz
 
 import (
-	// "golang.org/x/exp/mmap"
+	"golang.org/x/exp/mmap"
 	"encoding/binary"
 	"fmt"
 	"io/ioutil"
@@ -24,6 +24,7 @@ type Bucket struct {
 type BucketList []Bucket
 
 type Tube struct {
+	Name          string
 	Root          string
 	buckets       BucketList
 }
@@ -41,10 +42,8 @@ func (buckets BucketList) Less(i, j int) bool {
 // func intToHex(i int64) {
 // 	val, err := fmt.Sprintf("%016x", i)
 // 	if err != nil {
-
 // 	}
 // }
-
 // func hexToInt(s string) {
 // 	return fmt.Sprintf("%016x", i)
 // }
@@ -73,10 +72,12 @@ func ScanBuckets(root string)  BucketList{
 	return buckets
 }
 
-func NewTube(root string) *Tube {
+func NewTube(root string, name string) *Tube {
+	root = path.Join(root, name)
 	buckets := ScanBuckets(root)
 	sort.Sort(buckets)
 	tube := &Tube{
+		Name:    name,
 		Root:    root,
 		buckets: buckets,
 	}
@@ -85,7 +86,9 @@ func NewTube(root string) *Tube {
 
 func (self *Tube) GetBucket(offset int64) Bucket {
 	var bucket Bucket
-	pos := sort.Search(len(self.buckets), func(i int) bool {return self.buckets[i].Offset >= offset })
+	pos := sort.Search(len(self.buckets), func(i int) bool {
+		return self.buckets[i].Offset >= offset
+	})
 	bucket = self.buckets[pos]
 	return bucket
 }
@@ -95,10 +98,16 @@ func (self *Tube) GetWriteBucket(chunk_size int64) Bucket {
 	if chunk_size > MaxBucketSize {
 		panic("Chunk size bigger that MaxBucketSize")
 	}
+
+	// No bucket yet, create it
 	if len(self.buckets) == 0 {
-		return Bucket{0, 0}
+		new_bucket :=  Bucket{0, 0}
+		self.buckets = append(self.buckets, new_bucket)
+		return new_bucket
 	}
 
+	// Check if the last bucket has enough place left for the chunk
+	// size
 	last_bucket := self.buckets[len(self.buckets)-1]
 	if last_bucket.Size + chunk_size > MaxBucketSize {
 		new_bucket := Bucket{
@@ -112,8 +121,8 @@ func (self *Tube) GetWriteBucket(chunk_size int64) Bucket {
 }
 
 func (self *Tube) Append(data []byte, extra_indexes ...string) error {
-	bucket_id := self.GetWriteBucket(int64(len(data) * 8))
-	bucket_name := fmt.Sprintf("%016x", bucket_id)
+	bucket := self.GetWriteBucket(int64(len(data) * 8))
+	bucket_name := strconv.FormatInt(bucket.Offset, 16)
 	filename := path.Join(self.Root, bucket_name)
 	// Append data to bucket file
 	fh, err := os.OpenFile(filename + ".franz", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0650)
@@ -170,8 +179,50 @@ func (self *Tube) UpdateIndex(index_name string, offset []byte) error {
 	return nil
 }
 
-// func (self *Tube) Read(offset int64) ([]byte, error) {
-// }
+func (self *Tube) Read(offset int64) ([]byte, error) {
+	bucket := self.GetBucket(offset)
+	bucket_name := strconv.FormatInt(bucket.Offset, 16)
+	filename := path.Join(self.Root, bucket_name)
+	idx_fh, err := mmap.Open(filename + ".idx")
+	if err != nil {
+		return nil, err
+	}
+
+	// offset inside the bucket is relative to the offest of the
+	// bucket itself
+	chunk_offset := int32(offset - bucket.Offset)
+	// Each index item take 64bits (8 bytes)
+	nb_pos := idx_fh.Len() / 8
+	buff := make([]byte, 4)
+	pos := sort.Search(nb_pos, func(i int) bool {
+		idx_fh.ReadAt(buff, int64(i) * 8)
+		value := binary.LittleEndian.Uint32(buff)
+		return int32(value) >= chunk_offset
+	})
+
+	// pos contains the position in the index file of the requested
+	// offset
+	idx_fh.ReadAt(buff, int64(pos) * 8)
+	value := binary.LittleEndian.Uint32(buff)
+	if int32(value) != chunk_offset {
+		err = fmt.Errorf("Offset %q does not exists in %q", offset, self.Name)
+		return nil, err
+	}
+
+	// pos + 1 tell where the chunk stop
+	idx_fh.ReadAt(buff, int64(pos + 1) * 8)
+	value = binary.LittleEndian.Uint32(buff)
+	chunk_size := int32(value) - chunk_offset
+
+	// Read actual content
+	bucket_fh, err := mmap.Open(filename + ".franz")
+	if err != nil {
+		return nil, err
+	}
+	chunk_content := make([]byte, chunk_size)
+	bucket_fh.ReadAt(chunk_content, int64(chunk_offset))
+	return chunk_content, nil
+}
 
 // func (self *Tube) Info()  {
 // }
--- tube_test.go
+++ tube_test.go
@@ -22,7 +22,7 @@ func cleanup() {
 
 func TestInit(t *testing.T) {
 	cleanup()
-	tube := NewTube(TEST_DIR)
+	tube := NewTube(".", TEST_DIR)
 	if len(tube.buckets) > 0 {
 		t.Error("data dir not clean")
 	}
@@ -30,8 +30,10 @@ func TestInit(t *testing.T) {
 
 func TestAppend(t *testing.T) {
 	cleanup()
-	tube := NewTube(TEST_DIR)
-	err := tube.Append([]byte("hello"))
+	tube := NewTube(".", TEST_DIR)
+	hello := []byte("hello")
+	// Append with no tags
+	err := tube.Append(hello)
 	if err != nil {
 		t.Error(err)
 	}
@@ -39,13 +41,29 @@ func TestAppend(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-
 	if len(files) != 2 {
 		t.Error("Unexpected number of file")
 	}
-
 	if files[0].Size() != 5 {
 		t.Error("Unexpected file size")
 	}
 
+	//append with tags
+	err = tube.Append(hello, "ham", "spam")
+	if err != nil {
+		t.Error(err)
+	}
+	files, err = ioutil.ReadDir(TEST_DIR)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(files) != 4 {
+		t.Error("Unexpected number of file")
+	}
+
+	// Check that actual contant is there
+	content, err := tube.Read(0)
+	for pos, b := range content {
+		println(b,  hello[pos])
+	}
 }
