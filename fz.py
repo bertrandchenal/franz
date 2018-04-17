5f75577dbb0d245b672da6b90e53eca9f394c3ad
Bertrand Chenal <bertrandchenal@gmail.com>
2018-04-17 17:31:30 +0200

    Add follow option to fz.py; rename fz into franzd

--- fz.py
+++ fz.py
@@ -1,26 +1,40 @@
 from time import time
 import argparse
+import sys
 
 import websocket
+from websocket import _logging, WebSocketTimeoutException
+
 websocket.enableTrace(False)
-from websocket import _logging
 _logging._logger.setLevel('ERROR')
 
-def pub(ws, tube, payload):
-    ws.send('7:publish,%s:%s,%s:%s,' % (
-        len(tube), tube, len(payload), payload))
-    return ws.recv()
 
-def sub(ws, tube, offset=None):
+def pub(ws, tube, *lines):
+    if not lines:
+        lines = (l.strip() for l in sys.stdin)
+    for line in lines:
+        ws.send('7:publish,%s:%s,%s:%s,' % (
+            len(tube), tube, len(line), line))
+    ws.recv()
+
+
+def send_sub(ws, tube, offset):
     msg = '9:subscribe,%s:%s,' % (len(tube), tube)
     if offset:
         offset = str(offset)
         msg += '%s:%s,' % (len(offset), offset)
     ws.send(msg)
-    data = ws.recv()
-    yield data
-    while data:
-        data = ws.recv()
+
+def sub(ws, tube, offset=0, follow=False):
+    send_sub(ws, tube, offset)
+    if not follow:
+        ws.settimeout(1)
+    while True:
+        try:
+            data = ws.recv()
+            offset += len(data)
+        except WebSocketTimeoutException:
+            break
         yield data
 
 def bench(ws):
@@ -47,7 +61,8 @@ def main(cli):
     if action == 'pub':
         pub(ws, *args)
     elif action == 'sub':
-        print(sub(ws, *args))
+        for chunk in sub(ws, *args, follow=cli['follow']):
+            print(chunk)
     elif action == 'bench':
         bench(ws)
     else:
@@ -58,6 +73,9 @@ def main(cli):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('action', nargs='*', help='pub|sub')
+    parser.add_argument('-f', '--follow', action='store_true',
+                        help='Wait for new content when end of tube is reached',
+                        )
     cli = parser.parse_args()
 
     main(vars(cli))
